c45f7425a33cf391de3eaff00a951273348a3f3b
Chris Tufaro <ctufaro@gmail.com>
2019-04-25 16:57:45 -0400

    Adding components

--- src/Home.vue
+++ src/Home.vue
@@ -3,13 +3,12 @@
         <!-- Sidebar  -->
         <nav id="sidebar">
             <div class="sidebar-header">
-                <h3>Bootstrap Sidebar</h3>
+                <i class="fas fa-feather-alt"></i>
             </div>
 
             <ul class="list-unstyled components">
-                <p>Dummy Heading</p>
-                <li class="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                <!--<li>
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">My stocks</a>
                     <ul class="collapse list-unstyled" id="homeSubmenu">
                         <li>
                             <a href="#">Home 1</a>
@@ -21,47 +20,36 @@
                             <a href="#">Home 3</a>
                         </li>
                     </ul>
+                </li>-->
+                <li>
+                    <a href="#">My stocks <span class="float-right circle">22</span></a>                    
                 </li>
                 <li>
-                    <a href="#">About</a>
+                    <a href="#">Buy stocks</a>
+                </li>                                                
+                <li>
+                    <router-link to="/about">Pending orders</router-link>
                 </li>
                 <li>
-                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                    <ul class="collapse list-unstyled" id="pageSubmenu">
-                        <li>
-                            <a href="#">Page 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Page 3</a>
-                        </li>
-                    </ul>
+                    <a href="#">Insights</a>
                 </li>
                 <li>
-                    <a href="#">Portfolio</a>
+                    <a href="#">Settings</a>
                 </li>
                 <li>
-                    <a href="#">Contact</a>
-                </li>
+                    <a href="#">History</a>
+                </li>                                                                                                
             </ul>
 
-            <ul class="list-unstyled CTAs">
-                <li>
-                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
-                </li>
-                <li>
-                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
-                </li>
-            </ul>
+            <div class="fixed-bottom" style="padding-left:40px;padding-bottom:40px;">
+                <img alt="Avatar" class="avatar" src="./assets/images/avatar.png">
+            </div>
         </nav>
 
         <!-- Page Content  -->
         <div id="content">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-
                     <button type="button" id="sidebarCollapse" class="btn btn-info">
                         <i class="fas fa-align-left"></i>
                         <span>Toggle Sidebar</span>
@@ -69,7 +57,6 @@
                     <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
                     </button>
-
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
                             <li class="nav-item active">
@@ -88,12 +75,19 @@
                     </div>
                 </div>
             </nav>
-            <div class="row" >
-                <div class="col-md-3" style="background-color: salmon;">
-                   xxx
+            <div class="row justify-content-center h-100" >
+                <div class="col-md-3 searchcolumn" style="padding-left:0px;padding-right:0px;">
+                    <div class="searchcolumn-header">
+                       <input class="form-control searchtext" type="text" placeholder="Search" aria-label="Search">
+                    </div>
+                    <div class="searchcolumn-table">
+                        <SearchResults/>
+                    </div>
                 </div>
-                <div class="col-md-9" style="background-color: rosybrown;">
-                   yyy
+                <div class="col-md-9 pricecolumn">
+                   <div class="market">
+                        <div class="price">$227.75</div>                        
+                   </div>
                 </div>
              </div>
         </div>
@@ -101,184 +95,20 @@
 </template>
 
 <script>
+import SearchResults from '@/components/SearchResults.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    SearchResults
+  },
+  data() {
+    return{
+        greeting: 'Hi Dick'
+    }    
+  }
 }
 </script>
 
-<style scoped>
-/*
-    DEMO STYLE
-*/
 
-@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
-body {
-    font-family: 'Poppins', sans-serif;
-    background: #fafafa;
-}
-
-p {
-    font-family: 'Poppins', sans-serif;
-    font-size: 1.1em;
-    font-weight: 300;
-    line-height: 1.7em;
-    color: #999;
-}
-
-a,
-a:hover,
-a:focus {
-    color: inherit;
-    text-decoration: none;
-    transition: all 0.3s;
-}
-
-.navbar {
-    padding: 15px 10px;
-    background: #fff;
-    border: none;
-    border-radius: 0;
-    margin-bottom: 40px;
-    display:none;
-}
-
-.navbar-btn {
-    box-shadow: none;
-    outline: none !important;
-    border: none;
-}
-
-.line {
-    width: 100%;
-    height: 1px;
-    border-bottom: 1px dashed #ddd;
-    margin: 40px 0;
-}
-
-/* ---------------------------------------------------
-    SIDEBAR STYLE
------------------------------------------------------ */
-
-.wrapper {
-    display: flex;
-    width: 100%;
-    align-items: stretch;
-}
-
-#sidebar {
-    min-width: 250px;
-    max-width: 250px;
-    background: #7386D5;
-    color: #fff;
-    transition: all 0.3s;
-}
-
-#sidebar.active {
-    margin-left: -250px;
-}
-
-#sidebar .sidebar-header {
-    padding: 20px;
-    background: #6d7fcc;
-}
-
-#sidebar ul.components {
-    padding: 20px 0;
-    border-bottom: 1px solid #47748b;
-}
-
-#sidebar ul p {
-    color: #fff;
-    padding: 10px;
-}
-
-#sidebar ul li a {
-    padding: 10px;
-    font-size: 1.1em;
-    display: block;
-}
-
-#sidebar ul li a:hover {
-    color: #7386D5;
-    background: #fff;
-}
-
-#sidebar ul li.active>a,
-a[aria-expanded="true"] {
-    color: #fff;
-    background: #6d7fcc;
-}
-
-a[data-toggle="collapse"] {
-    position: relative;
-}
-
-.dropdown-toggle::after {
-    display: block;
-    position: absolute;
-    top: 50%;
-    right: 20px;
-    transform: translateY(-50%);
-}
-
-ul ul a {
-    font-size: 0.9em !important;
-    padding-left: 30px !important;
-    background: #6d7fcc;
-}
-
-ul.CTAs {
-    padding: 20px;
-}
-
-ul.CTAs a {
-    text-align: center;
-    font-size: 0.9em !important;
-    display: block;
-    border-radius: 5px;
-    margin-bottom: 5px;
-}
-
-a.download {
-    background: #fff;
-    color: #7386D5;
-}
-
-a.article,
-a.article:hover {
-    background: #6d7fcc !important;
-    color: #fff !important;
-}
-
-/* ---------------------------------------------------
-    CONTENT STYLE
------------------------------------------------------ */
-
-#content {
-    width: 100%;
-    padding-left: 15px;
-    padding-right: 20px;
-    min-height: 100vh;
-    transition: all 0.3s;
-}
-
-/* ---------------------------------------------------
-    MEDIAQUERIES
------------------------------------------------------ */
-
-@media (max-width: 768px) {
-    #sidebar {
-        margin-left: -250px;
-    }
-    #sidebar.active {
-        margin-left: 0;
-    }
-    #sidebarCollapse span {
-        display: none;
-    }
-    .navbar {
-        display:block;
-        margin-bottom: 5px;
-    }
-}
-</style>
+<style src="../src/assets/css/home.css"></style>
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="flex-grid tricker-cont">
+            <div class="col">
+                <div class="tricker">AAPL</div>
+                <div class="shares">120 SHARES</div>
+            </div>
+            <div class="col">--------</div>
+            <div class="col">
+                <button type="button" class="btn btn-secondary btn-sm">$105.67</button>
+            </div>
+        </div>
+        <div class="flex-grid tricker-cont">
+            <div class="col">
+                <div class="tricker">AAPL</div>
+                <div class="shares">120 SHARES</div>
+            </div>
+            <div class="col">--------</div>
+            <div class="col">
+                <button type="button" class="btn btn-info btn-sm">$105.67</button>
+            </div>
+        </div>        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchResults'  
+}
+</script>
+
+<style src="../../src/assets/css/searchresults.css"></style>
