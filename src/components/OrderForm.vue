dbe75441e1800e14fb52420e9ed8d9ea8cf67dc3
Chris Tufaro <ctufaro@gmail.com>
2019-05-08 16:21:16 -0400

    removing all jquery and really long text in the order form

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -1,28 +1,5 @@
 <template>
-<div>
     <div>Order Form</div>
-    <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae condimentum nibh. Mauris sed erat lacinia, imperdiet massa vel, sodales diam. Integer quam est, cursus eget lectus vitae, pharetra ullamcorper metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam blandit urna non tristique suscipit. Pellentesque consequat nibh consequat leo sollicitudin porttitor. Integer sollicitudin a ante nec porttitor.
-
-Aliquam porttitor vel massa nec porttitor. Maecenas tempus felis turpis, vitae lobortis diam mollis at. Vivamus vitae quam in lorem venenatis consectetur ac a augue. Maecenas porta pharetra dui a ullamcorper. Etiam libero urna, sagittis non feugiat id, eleifend ac lectus. Sed et diam quam. Praesent viverra euismod ligula ut dignissim. Phasellus vitae velit non neque ullamcorper pharetra. Vivamus commodo molestie quam, at scelerisque massa varius ac. Cras arcu urna, tincidunt at rhoncus vel, sollicitudin rutrum neque. Cras vitae elit vel neque rhoncus imperdiet in sed odio. Suspendisse quis nunc lectus. Maecenas risus augue, porta vitae pharetra pellentesque, consequat at lectus. Nulla urna nunc, malesuada nec commodo ac, efficitur a enim.
-
-In eu finibus mi, vel facilisis dui. Aenean posuere, tellus eu porttitor congue, diam dui eleifend sapien, eget ultrices lacus metus at dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris malesuada mi ac nunc aliquam, in pellentesque erat sollicitudin. Cras tincidunt ex eu felis dignissim mollis. Ut tempor, orci et porttitor tincidunt, velit nibh semper magna, et ullamcorper sapien lacus et leo. Aenean dictum lacinia justo, eget elementum ligula interdum a.
-
-Praesent pellentesque faucibus arcu sed elementum. Sed at vehicula turpis, id ultrices lorem. Donec nisl risus, porta vel interdum congue, malesuada tincidunt nisi. Vestibulum sit amet pulvinar dui. Cras ac pretium enim. Donec vitae arcu sit amet felis rutrum sagittis vel aliquet lacus. Aliquam at elementum augue, vel hendrerit tortor. Donec maximus facilisis volutpat.
-
-Vestibulum urna arcu, tincidunt ut interdum eu, gravida sit amet nulla. Donec pellentesque ac lectus eget condimentum. Donec sed diam ipsum. Sed vel eros eros. In sed mi quis tellus pulvinar maximus. Pellentesque tincidunt imperdiet nisi vitae imperdiet. Ut nec ornare lectus, vitae pulvinar tellus.  
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae condimentum nibh. Mauris sed erat lacinia, imperdiet massa vel, sodales diam. Integer quam est, cursus eget lectus vitae, pharetra ullamcorper metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam blandit urna non tristique suscipit. Pellentesque consequat nibh consequat leo sollicitudin porttitor. Integer sollicitudin a ante nec porttitor.
-
-Aliquam porttitor vel massa nec porttitor. Maecenas tempus felis turpis, vitae lobortis diam mollis at. Vivamus vitae quam in lorem venenatis consectetur ac a augue. Maecenas porta pharetra dui a ullamcorper. Etiam libero urna, sagittis non feugiat id, eleifend ac lectus. Sed et diam quam. Praesent viverra euismod ligula ut dignissim. Phasellus vitae velit non neque ullamcorper pharetra. Vivamus commodo molestie quam, at scelerisque massa varius ac. Cras arcu urna, tincidunt at rhoncus vel, sollicitudin rutrum neque. Cras vitae elit vel neque rhoncus imperdiet in sed odio. Suspendisse quis nunc lectus. Maecenas risus augue, porta vitae pharetra pellentesque, consequat at lectus. Nulla urna nunc, malesuada nec commodo ac, efficitur a enim.
-
-In eu finibus mi, vel facilisis dui. Aenean posuere, tellus eu porttitor congue, diam dui eleifend sapien, eget ultrices lacus metus at dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris malesuada mi ac nunc aliquam, in pellentesque erat sollicitudin. Cras tincidunt ex eu felis dignissim mollis. Ut tempor, orci et porttitor tincidunt, velit nibh semper magna, et ullamcorper sapien lacus et leo. Aenean dictum lacinia justo, eget elementum ligula interdum a.
-
-Praesent pellentesque faucibus arcu sed elementum. Sed at vehicula turpis, id ultrices lorem. Donec nisl risus, porta vel interdum congue, malesuada tincidunt nisi. Vestibulum sit amet pulvinar dui. Cras ac pretium enim. Donec vitae arcu sit amet felis rutrum sagittis vel aliquet lacus. Aliquam at elementum augue, vel hendrerit tortor. Donec maximus facilisis volutpat.
-
-Vestibulum urna arcu, tincidunt ut interdum eu, gravida sit amet nulla. Donec pellentesque ac lectus eget condimentum. Donec sed diam ipsum. Sed vel eros eros. In sed mi quis tellus pulvinar maximus. Pellentesque tincidunt imperdiet nisi vitae imperdiet. Ut nec ornare lectus, vitae pulvinar tellus.
-    </p>
-</div>    
 </template>
 
 <script>
