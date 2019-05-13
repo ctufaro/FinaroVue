0b8aafd9ae30fcc8b216bfe8ff313bfe8360f352
Chris Tufaro <ctufaro@gmail.com>
2019-05-13 15:36:58 -0400

    Layout changes

--- src/components/OrderForm.vue
+++ src/components/OrderForm.vue
@@ -2,17 +2,23 @@
     <div class="order-form">        
         <div>
             <div class="line-title">Price</div>
-            <input type="text" class="text-line" maxlength="13" placeholder="$0.00"/>
+            <div class="text-line">
+                <input type="tel" min="0" style="width:75%;text-align:right;" placeholder="$0" v-mask="'#########'" v-model="price"/>
+                <span >.</span>
+                <input type="tel" min="0" style="width:50px;" class="three-border" placeholder="00" v-mask="'##'" v-model="cents"/>
+            </div>
         </div>
         <div>
             <div class="line-title">Shares</div>
-            <input type="text" class="text-line" maxlength="13" placeholder="0"/>
+            <input type="tel" class="text-line w-100" min="0" placeholder="0" v-mask="'############'" v-model="shares"/>
         </div>
+        <div>
+            <div class="line-title">Cost</div>
+            <input class="text-line w-100" min="0" readonly="true" value="$100.50"/>
+        </div>        
         <div style="padding-top:64px;">
             <div class="btn-group w-100 searchcolumn-filters" role="group" aria-label="Basic example">
                 <button type="button" class="btn btn-primary">Place Order</button>
-                &nbsp;&nbsp;
-                <button type="button" class="btn btn-warning">Cancel</button>
             </div>
         </div>
     </div>
@@ -20,7 +26,12 @@
 
 <script>
 export default {
-
+name: 'OrderFrom',
+    data: () => ({
+      price:'',
+      cents:'',
+      shares:''
+    })
 }
 </script>
 
--- src/pages/Exchange/main.js
+++ src/pages/Exchange/main.js
@@ -5,10 +5,12 @@ import vueHeadful from 'vue-headful';
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import router from '../../router.js';
+import VueMask from 'v-mask'
 
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2, {confirmButtonColor: '#0079FF'});
 Vue.use(VueRouter);
+Vue.use(VueMask);
 Vue.component('vue-headful', vueHeadful);
 
 new Vue({
