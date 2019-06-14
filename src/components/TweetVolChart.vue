675bd99d4ec4e0a0f9978ca313bd9e54e06d9d16
Chris Tufaro <ctufaro@gmail.com>
2019-06-14 13:04:45 -0400

    Bar

--- src/components/TweetVolChart.vue
+++ src/components/TweetVolChart.vue
@@ -1,6 +1,6 @@
 <script>
-import {Bubble} from 'vue-chartjs'
-import moment from 'moment'
+import {Bar} from 'vue-chartjs'
+//import moment from 'moment'
 
 let percentColors = [
     { pct: 0.0, color: { r: 239, g: 73, b: 66 } },
@@ -8,7 +8,72 @@ let percentColors = [
     { pct: 1.0, color: { r: 74, g: 195, b: 74 } } ];
 
 export default {
-  extends: Bubble,
+  extends: Bar,
+  data () {
+    return {
+      datacollection: {
+        //Data to be represented on x-axis
+        labels: ['6/12 12:40', '6/12 13:40', '6/12 14:40', '6/12 15:40', '6/12 16:40', '6/12 17:40'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: [this.getColor(.50),this.getColor(.62),this.getColor(.89),this.getColor(.99),this.getColor(.19),this.getColor(.02)],
+            //pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#249EBF',
+            data: [2563838,2618977,2626350,2618977,2626350,2563838]
+          }
+        ]
+      },
+      //Chart.js options that controls the appearance of the chart
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              callback: function(value) {               
+                if(value >= 1000000){
+                  return (value/1000000)+'m';
+                }
+                else if (value >= 1000){
+                  return (value/1000)+'k';
+                }
+                return value;
+              },              
+            },
+            gridLines: {
+              display: true
+            },
+            scaleLabel: {
+              display: false,
+              fontSize: 14,
+              fontStyle:'bold',
+              labelString: "Tweet Volume",
+            }            
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true,
+          mode: 'single',
+          callbacks: {
+            label: function(tooltipItems) {
+              return 'Tweets: ' + tooltipItems.yLabel;
+            }
+          }
+        },        
+        responsive: true,
+        maintainAspectRatio: true
+      }
+    }
+  },  
   methods:{
     getColor: function(pct) {
         for (var i = 1; i < percentColors.length - 1; i++) {
@@ -27,73 +92,12 @@ export default {
                 g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
                 b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
             };
-            return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+            return 'rgba(' + [color.r, color.g, color.b].join(',') + ',0.8)';
             // or output as hex if preferred
     }
   },
   mounted () {
-    //console.log(moment(new Date('2019-06-12 12:46:16.187')).format('M/DD h:mmA'));
-    this.renderChart({
-        //labels: ['t1', 't2', 't3', 't4', 't5', 't6', 't7'],
-        //x=time, y=tweet volume, r=tweet volume, backgroundColor: sentiment
-        datasets: [
-          {
-            backgroundColor: this.getColor(.50),
-            data: [{
-              x: moment(new Date('2019-06-12 12:40:16.187')),
-              y: 2563838,
-              r: 32
-            }]
-          },
-          {
-            backgroundColor: this.getColor(.62),
-            data: [{
-              x: moment(new Date('2019-06-12 13:00:37.050')),
-              y: 2618977,
-              r: 34
-            }]
-          },
-          {
-            backgroundColor: this.getColor(.89),
-            data: [{
-              x: moment(new Date('2019-06-12 13:20:01.907')),
-              y: 2626350,
-              r: 34
-            }]
-          }
-        ]
-      }, {
-      responsive: true,
-      maintainAspectRatio: true,
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [{
-          type: 'time',
-          time: {
-            displayFormats: this.$datefmt
-          },
-          scaleLabel: {
-            display: true,
-            fontSize: 14,
-            fontStyle:'bold',
-            labelString: "Time",
-          },
-          ticks: {
-            suggestedMax: moment(new Date('2019-06-13 15:46:16.187'))
-          }                    
-        }],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            fontSize: 14,
-            fontStyle:'bold',
-            labelString: "Tweet Volume",
-          }          
-        }]        
-      }
-    })
+    this.renderChart(this.datacollection, this.options)
   }
 }
 </script>
