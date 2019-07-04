c982f883efacb105d7c94e555fd56e3af5bb9c5a
christufaro <ctufaro@gmail.com>
2019-07-04 08:08:46 -0400

    Profile Page

--- src/components/MyTrends.vue
+++ src/components/MyTrends.vue
@@ -7,11 +7,21 @@
                         <v-avatar size="96" class="mr-4">
                             <img src="@/assets/images/avatar-chris-main.jpg" alt="Avatar">
                         </v-avatar>                        
-                    </v-flex>   
-                    <span class="font-weight-bold text-secondary" style="font-size:20px;">Christopher Tufaro</span>                 
-                </v-card-text>                
+                    </v-flex>
+                    <span class="font-weight-bold text-secondary" style="font-size:20px;">Christopher Tufaro</span>
+                </v-card-text>
+                <v-expansion-panel>
+                    <v-expansion-panel-content v-for="(item,i) in ['Ordered Trends','Sold Trends','Awards','Friends']" :key="i">
+                        <template v-slot:header>
+                            <div><strong>{{item}}</strong></div>
+                        </template>
+                        <v-card>
+                            <v-card-text></v-card-text>
+                        </v-card>
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
             </v-card>
-        </v-layout>        
+        </v-layout>
     </v-container>
 </template>
 
@@ -22,5 +32,8 @@ export default {
 </script>
 
 <style>
-
+.v-expansion-panel{
+    -webkit-box-shadow: none;
+    box-shadow: none;
+}
 </style>
